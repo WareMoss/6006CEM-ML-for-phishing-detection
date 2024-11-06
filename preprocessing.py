f15612a201c0f877d23fbc3d1e45492f6fb2e8be
Ryan <115543058+WareMoss@users.noreply.github.com>
2024-11-06 16:10:33 +0000

    Update preprocessing.py
    final version

--- preprocessing.py
+++ preprocessing.py
@@ -1,40 +1,46 @@
-from imports import *
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
 def main():
-    df = pd.read_csv('PhiUSIIL_Phishing_URL_DatasetU.csv', header=0, low_memory=False)
-    # reads in the cleaned dataset
+    df = pd.read_csv('PhiUSIIL_Phishing_URL_DatasetU.csv', header=0, low_memory=False, nrows=10000)
+    # reads in the cleaned dataset as df using pandas
     X = df.drop('label', axis=1)
     # Training dataset only contains the values we want to find a link between, label is the answer to if a site was phishing or not
-    # hiding this  avoids overfitting where the model simply memorizes the dataset instead of looking for a pattern
+    # hiding this avoids overfitting where the model simply memorizes the dataset instead of looking for a pattern
+    # or data leakage where the model knows the values
     y = df['label']
     # 1 = legit 0 = phishing
     # y contains the answers to compare the results of the model against
-    encoder = OneHotEncoder(sparse_output=False, drop='first')  # drop='first' avoids multicollinearity
-    # Apply the encoder to the HTTPS column
-    # this turns the catagorical data into an integer to be processed
-    https_encoded = encoder.fit_transform(df[['HTTPS']])
-    # this selects the HTTPS columnm from the dataset and turns it into its own dataframe
-    # this is because onehotencoder expects a dataframe as an input
-    # fit_transform() is a method that learns the unique catagories present in hte HTTPS column, these are represented as 1 and 0
-    # then the data is transformed from catagorical into one-hot encoded format
-    # this creates new binary columns (0 or 1) for each unique catagory
-    https_df = pd.DataFrame(https_encoded, columns=encoder.get_feature_names_out(['HTTPS']))
-    # print(https_df)
-    # this converts the now one hot encoded array https_encoded into a dataframe and names the new column HTTPS
-    # Drop the original HTTPS column from the DataFrame
-    X = X.drop('HTTPS', axis=1)
-    # print(X)
-    # drops the old HTTPS column from the dataset
-    X = pd.concat([X.reset_index(drop=True), https_df.reset_index(drop=True)], axis=1)
-    # print(X['HTTPS_https'])
-    # this print statement confirms that the column has correctly been added to the X data frame
-    # reset_index(drop=True) this prevents the old index from being addes as a new column in the dataframe, ensures proper alignment 
-    # # https_df.reset_index(drop=True) does the same
-    # Combine the original DataFrame with the new one-hot encoded DataFrame
-    # axis=1 means the concatination should be done column wise
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    # X_train is the features the model is trying to find a relationship between to be trained on
-    # X_test is 'unseen' so the model will use what its learnt in train to try and predict the label value for this set of data
-    # y_train is the label feature so the model can learn what the expected output should be 
-    # y_test is the features for the X_test dataset and the predictions can be compared to the actual values 
-    return X_train, X_test, y_train, y_test
+    if 'TLD_101' not in df.columns:
+        # if the onehotencoded column isnt in the dataset, perform onehotencoding on the TLD column
+        encoder = OneHotEncoder(sparse_output=False, drop='first')  # drop='first' avoids multicollinearity
+        # Apply the encoder to all values in the TLD column
+        # this turns the catagorical data into an integer to be processed
+        # so that the Ml algorithm can work
+        TLD_encoded = encoder.fit_transform(X[['TLD']])
+        # this selects the HTTTLDPS columnm from the dataset and turns it into its own dataframe
+        # this is because onehotencoder expects a dataframe as an input
+        # fit_transform() is a method that learns the unique catagories present in hte HTTPS column, these are represented as 1 and 0
+        # then the data is transformed from catagorical into one-hot encoded format
+        # this creates new binary columns (0 or 1) for each unique catagory
+        TLD_df = pd.DataFrame(TLD_encoded, columns=encoder.get_feature_names_out(['TLD']))
+        # this converts the now one hot encoded array TLD_encoded into a dataframe and names the new columns TLD_...
+        X = X.drop('TLD', axis=1)
+        # drop original row
+        X = pd.concat([X.reset_index(drop=True), TLD_df.reset_index(drop=True)], axis=1)
+        # combines the old dataset and the new dataset
+        df_combined = pd.concat([X, y], axis=1)
+        df_combined.to_csv('PhiUSIIL_Phishing_URL_DatasetU.csv', index=False)
+        # saves the results into the cleaned csv file
+    else:
+        # if the data has already been encoded
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+        # split the data into training 75% and testing 25%
+        # X_train is the features the model is trying to find a relationship between to be trained on
+        # X_test is 'unseen' so the model will use what its learnt in train to try and predict the label value for this set of data
+        # y_train is the label feature so the model can learn what the expected output should be 
+        # y_test is the features for the X_test dataset and the predictions can be compared to the actual values 
+        return X_train, X_test, y_train, y_test
 X_train, X_test, y_train, y_test = main()
+if __name__ == "__main__":
+    main()
